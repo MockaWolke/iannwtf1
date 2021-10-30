c82c64dfa8e478fd53695148be31add3238c703a
Louis Kapp <lkapp@uni-osnabrueck.de>
2021-10-30 15:38:18 +0200

    Better param naming

--- activation.py
+++ activation.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-def sigmoid(di):                            # sigmoid activation func
-    return 1 / (1 + np.exp(-di))
+def sigmoid(d):                            # sigmoid activation func
+    return 1 / (1 + np.exp(-d))
 
-def sigmoidprime(di):                       # derivative of the sigmoid activation func
-    return sigmoid(di) * (1 - sigmoid(di))
+def sigmoidprime(d):                       # derivative of the sigmoid activation func
+    sig = sigmoid(d)
+    return sig * (1 - sig)
+
+if __name__ == "__main__":
+    print(sigmoid(7))
+    print(sigmoidprime(7))
