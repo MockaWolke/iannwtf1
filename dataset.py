b10a86ada5b2834917b11ccc295693b9e611b3f4
Louis Kapp <lkapp@uni-osnabrueck.de>
2021-10-30 15:38:41 +0200

    Finished

--- dataset.py
+++ dataset.py
@@ -1,3 +1,9 @@
 import numpy as np
 
-inputs = np.random.rand(100)
+inputs = np.array([(1,0),(1,1),(0,0),(0,1)], dtype=np.bool_)
+
+and_lables = np.array([i[0] & i[1] for i in inputs])
+or_lables = np.array([i[0] | i[1] for i in inputs])
+nand_lables = ~ and_lables
+nor_lables = ~ or_lables
+xor_lables = nand_lables & or_lables
