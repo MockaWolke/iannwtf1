a5154cd54888b0e734fc38b8d4bf15711f3440b5
Yannik101010 <80115275+Yannik101010@users.noreply.github.com>
2021-11-07 16:27:43 +0100

    Update eval.py, training.py, Change name training.ipynb to visualization.ipynb
    Some more comments and name change.

--- backprop_from_scratch/training.py
+++ backprop_from_scratch/training.py
@@ -1,9 +1,21 @@
+"""
+Training skript
+
+Provides functions for training our MLP and plotting results.
+"""
+
 import numpy as np
 from eval import  accuracy, squared_error
 import matplotlib.pyplot as plt
 
+
 def epoch(mlp, data):
-    
+    """
+    Function to pass data forward and backward through our MLP.
+    ## Params
+        -   mlp = object of class MLP
+        -   data = data from dataset.py ("and", "or", "nand", "nor, "xor)
+    """
     input_values, targets = data
 
     accuracy_of_loss = 0
@@ -19,6 +31,13 @@ def epoch(mlp, data):
     
 
 def train(mlp,data, times= 10000):
+    """
+    Prints and plots our MLP's predictions.
+    ## Params
+        -   mlp = object of class MLP
+        -   data = data from dataset.py ("and", "or", "nand", "nor, "xor)
+        -   times = iterations
+    """
     erg=[]
     x_axes= np.linspace(0,10000,num=times)
     for i in range(0,len(x_axes)):
@@ -28,11 +47,16 @@ def train(mlp,data, times= 10000):
     plt.show()
     
     for i,logic_input in enumerate(data[0]):
-        print(f"Our data is: {logic_input}, the target is {data[1][i]}, our mlp returns {float(mlp.forward_step(logic_input))}")
+        print(f"Our data is: {logic_input}, the target is {data[1][i]}, our mlp returns {float(mlp.forward_step(logic_input)[0])}")
 
 
 def live_epoch(mlp,data):
-    
+    """
+    Function to pass data forward and backward through our MLP, but for live visualization.
+    ## Params
+        -   mlp = object of class MLP
+        -   data = data from dataset.py ("and", "or", "nand", "nor, "xor)
+    """
     input_values, targets = data
 
     accuracy_of_loss = 0
@@ -48,4 +72,3 @@ def live_epoch(mlp,data):
         mlp.backprob_step(targets[i])
         
     return accuracy_of_loss / len(input_values) , sq_error /len(input_values)
-
