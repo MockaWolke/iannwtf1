06045325cae958436cea732888ac9a2cef8843b5
Louis Kapp <lkapp@uni-osnabrueck.de>
2021-11-12 18:45:02 +0100

    Final changes

--- HW3_bacteria/preprocessing.py
+++ HW3_bacteria/preprocessing.py
@@ -23,9 +23,10 @@ def onehotify(tensor):
 def prep_data(ds_train, ds_test):
     """Applies one-hot encoding on our dataset."""
     # map input to onehotify and targets to onehot vectors
-    new_ds_train = ds_train.map(lambda input, target: (onehotify(input),tf.one_hot(target,10))) 
+    new_ds_train = ds_train.map(lambda input, target: (onehotify(input),tf.one_hot(target,10)))
     # map input to onehotify and targets to onehot vectors
-    new_ds_test = ds_test.map(lambda input, target: (onehotify(input),tf.one_hot(target,10))) 
-    new_ds_train = new_ds_train.shuffle(10000).batch(1000).prefetch(tf.data.AUTOTUNE)
-    new_ds_test = new_ds_test.shuffle(100).batch(100).prefetch(tf.data.AUTOTUNE)
+    new_ds_test = ds_test.map(lambda input, target: (onehotify(input),tf.one_hot(target,10)))
+    # different batch and shuffle sizes are useful for our train and test dataset
+    new_ds_train = new_ds_train.shuffle(10000).batch(1000).prefetch(tf.data.AUTOTUNE) # tf.data.AUTOTUNE tunes our prefetch-size
+    new_ds_test = new_ds_test.shuffle(100).batch(100).prefetch(tf.data.AUTOTUNE) # dynamically at runtime to make it as fast as possible 
     return new_ds_train, new_ds_test
