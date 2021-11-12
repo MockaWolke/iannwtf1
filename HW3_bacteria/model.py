4c748908d6ee258ae484185aa19c617b1ddebb26
Yannik101010 <80115275+Yannik101010@users.noreply.github.com>
2021-11-12 13:55:03 +0100

    Updated files
    Added comments and classes

--- HW3_bacteria/model.py
+++ HW3_bacteria/model.py
@@ -1,5 +1,7 @@
 """
 Class Model
+
+Defines a Model
 """
 
 import numpy as np
@@ -8,10 +10,11 @@ import tensorflow_datasets as tfds
 
 
 class MyModel(tf.keras.Model):
-    """"""
+    """MyModel represents a Tensorflow Model, inherits from keras.Model."""
     
     
     def __init__(self):
+        """Constructor, calls super Constructor, initializes 2 hidden layers and an output layer."""
         super(MyModel, self).__init__()
         self.hidden1 = Dense_Layer()
         self.hidden2 = Dense_Layer()
@@ -20,6 +23,11 @@ class MyModel(tf.keras.Model):
         
     @tf.function
     def call(self, inputs):
+        """
+        Update weights/biases of each layer (calls call of each neuron).
+        ## Params
+            - inputs = inputs for network
+        """
         x = self.hidden1(inputs)
         x = self.hidden2(x)
         x = self.out(x)
--- HW3_bacteria/preprocessing.py
+++ HW3_bacteria/preprocessing.py
@@ -8,7 +8,9 @@ import tensorflow_datasets as tfds
 
 
 def onehotify(tensor):
+    """One-Hot encoding for our dataset."""
     vocab = {'A':'1', 'C':'2', 'G':'3', 'T':'0'}
+    # for loop to replace the respective letter with a number
     for key in vocab.keys():
         tensor = tf.strings.regex_replace(tensor, key, vocab[key])
     split = tf.strings.bytes_split(tensor)
@@ -19,8 +21,11 @@ def onehotify(tensor):
 
 
 def prep_data(ds_train, ds_test):
-    new_ds_train = ds_train.map(lambda input, target: (onehotify(input),tf.one_hot(target,10))) # map input to onehotify and targets to onehot vectors
-    new_ds_test = ds_test.map(lambda input, target: (onehotify(input),tf.one_hot(target,10))) # map input to onehotify and targets to onehot vectors
+    """Applies one-hot encoding on our dataset."""
+    # map input to onehotify and targets to onehot vectors
+    new_ds_train = ds_train.map(lambda input, target: (onehotify(input),tf.one_hot(target,10))) 
+    # map input to onehotify and targets to onehot vectors
+    new_ds_test = ds_test.map(lambda input, target: (onehotify(input),tf.one_hot(target,10))) 
     new_ds_train = new_ds_train.shuffle(10000).batch(1000).prefetch(tf.data.AUTOTUNE)
     new_ds_test = new_ds_test.shuffle(100).batch(100).prefetch(tf.data.AUTOTUNE)
     return new_ds_train, new_ds_test
